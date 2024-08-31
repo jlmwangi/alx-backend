8d21f2ff8ecbfbdf5956a81617f7fa13fe3ffbf3
mwangii <jlmwangi254@gmail.com>
2024-08-31 21:43:49 +0300

    help simply

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''function returning an index range on start and end indexes
+based on the page and page_size'''
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''takes two args page and pagesize which are used to determine
+    the point at which
+    it should start indexing and which to end the indexing'''
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
