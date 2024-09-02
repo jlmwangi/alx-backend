6811bb9bba27ad1b0c9b4abecc18867687b79eb0
mwangii <jlmwangi254@gmail.com>
2024-09-02 15:24:29 +0300

    paginate simpy

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -18,7 +18,7 @@ class Server:
     def index_range(self, page: int, page_size: int) -> tuple:
         '''helper method to calcaulate starting and ending indexes'''
         start_index = (page - 1) * page_size
-        end_index = start_index + page_size
+        end_index = page * page_size  #start_index + page_size
         return start_index, end_index
 
     def dataset(self) -> List[List]:
