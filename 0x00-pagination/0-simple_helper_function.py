0230a558c85017532df17230598ff0ef4ada4fe5
mwangii <jlmwangi254@gmail.com>
2024-09-02 15:18:31 +0300

    paginate simply

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,5 +8,5 @@ def index_range(page: int, page_size: int) -> tuple:
     the point at which
     it should start indexing and which to end the indexing'''
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size  # page * page_size
     return start_index, end_index
---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''function that returns appropriate page of the dataset'''
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def index_range(self, page: int, page_size: int) -> tuple:
+        '''helper method to calcaulate starting and ending indexes'''
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''return appropriate page or correct list of rows'''
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+
+        assert page > 0
+        assert page_size > 0
+
+        data = self.dataset()  # load dataset
+        start_index, end_index = self.index_range(page, page_size)
+
+        if start_index >= len(data):  # check if strt index is out of range
+            return []
+
+        return data[start_index:end_index]
