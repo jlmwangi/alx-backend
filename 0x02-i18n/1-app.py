f9cf0343037ad3088d05a8fca5fb3d57bbff4e41
mwangii <jlmwangi254@gmail.com>
2024-10-01 11:45:57 +0300

    try task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''basic flask app'''
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app: Flask = Flask(__name__)
+app.Config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.Config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+babel = Babel(app)
+
+
+class Config():
+    '''class to configure languages and timezone'''
+    LANGUAGES = ["en", "fr"]
+
+
+@app.route('/')
+def home():
+    '''return an index page'''
+    return render_template('templates/1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
