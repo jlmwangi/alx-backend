ac4e31c083017b9fd967b3e65c72aaa46563ff18
mwangii <jlmwangi254@gmail.com>
2024-10-01 10:26:02 +0300

    setup basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,12 +3,13 @@
 
 
 from flask import Flask, render_template
+from flask.typing import ResponseReturnValue
 
 app: Flask = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home() -> ResponseReturnValue:
     '''return an index page'''
     return render_template('templates/0-index.html')
 
