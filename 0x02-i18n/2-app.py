314a2e3d1b1738108dfa0a78d967eaf7be9fdde8
mwangii <jlmwangi254@gmail.com>
2024-10-01 23:17:43 +0300

    get locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -29,7 +29,7 @@ def get_locale():
     #user = getattr(g, 'user', None)
     #if user is Not None:
     #    return user.locale
-    return request.accept_languages.best_match(['en', 'fr'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
