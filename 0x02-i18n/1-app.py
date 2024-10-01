19f5556fff69032650a01ac3a05b0dc209dce605
mwangii <jlmwangi254@gmail.com>
2024-10-01 20:38:18 +0300

    trial two

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,16 +6,16 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app: Flask = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+#  app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+#  app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 babel = Babel(app)
 
 
 class Config:
     '''class to configure languages and timezone'''
     LANGUAGES = ["en", "fr"]
-    #  'BABEL_DEFAULT_LOCALE' = 'en'
-    #  'BABEL_DEFAULT_TIMEZONE' = 'UTC'
+    'BABEL_DEFAULT_LOCALE' == 'en'
+    'BABEL_DEFAULT_TIMEZONE' == 'UTC'
 
 
 app.config.from_object(Config)
