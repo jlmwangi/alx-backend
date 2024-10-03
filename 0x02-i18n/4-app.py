985c5a0ab2851ccbecd87204bad0888ac463c4a0
mwangii <jlmwangi254@gmail.com>
2024-10-03 03:37:31 +0300

    Bonjour

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,10 +23,9 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     '''get_locale function to determine best match with supported languages'''
-    locale = request.args.get('locale')
-    if locale:  # if locale in query parameters
-        if locale in app.config['LANGUAGES']:  # if is an acceptable locale
-            return request.accept_languages.best_match(locale)
+    locale = request.args.get('locale')  #check if locale in query parameters
+    if locale and locale in app.config['LANGUAGES']:  # validate locale
+            return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
