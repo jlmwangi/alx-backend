45faf3175ed46edcd49b0e5c04c8c0aa12e8a462
mwangii <jlmwangi254@gmail.com>
2024-10-03 03:32:00 +0300

    fixed marks

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''basic flask app'''
+
+
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, gettext as _
+
+app = Flask(__name__)
+
+
+class Config:
+    '''class to configure languages and timezone'''
+    LANGUAGES = ["en", "fr"]
+
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''get_locale function to determine best match with supported languages'''
+    locale = request.args.get('locale')
+    if locale:  # if locale in query parameters
+        if locale in app.config['LANGUAGES']:  # if is an acceptable locale
+            return request.accept_languages.best_match(locale)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    '''return an index page'''
+    home_title = _('home_title')
+    home_header = _('home_header')
+    return render_template('4-index.html',
+                           home_title=home_title, home_header=home_header)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
